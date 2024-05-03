f614820b3b75f85a705ce5577081c63876a9d307
vishal <vishalpote9090@gmail.com>
2024-05-03 15:13:40 +0530

    change url's

--- Frontend/src/utils/apiRoute.js
+++ Frontend/src/utils/apiRoute.js
@@ -1,19 +1,19 @@
 //this is the host url
-export const host=`https://bookstore-1gta.onrender.com`;
+export const host=`${host}`;
 
 // this all user url
-export const loginRoute=`https://bookstore-1gta.onrender.com/user/login`;
-export const signupRoute = `https://bookstore-1gta.onrender.com/user/signup`;
-export const contactRoute = `https://bookstore-1gta.onrender.com/user/contact`;
-export const problemRoute = `https://bookstore-1gta.onrender.com/user/problem`;
-export const forgotpasswordRoute = `https://bookstore-1gta.onrender.com/user/forgotpassword`;
-export const resetpasswordRoute = `https://bookstore-1gta.onrender.com/user/resetpassword`;
-export const resetpasswordotpRoute = `https://bookstore-1gta.onrender.com/user/resetpasswordotp`;
+export const loginRoute=`${host}/user/login`;
+export const signupRoute = `${host}/user/signup`;
+export const contactRoute = `${host}/user/contact`;
+export const problemRoute = `${host}/user/problem`;
+export const forgotpasswordRoute = `${host}/user/forgotpassword`;
+export const resetpasswordRoute = `${host}/user/resetpassword`;
+export const resetpasswordotpRoute = `${host}/user/resetpasswordotp`;
 
 //this is the book and the services urls
-export const bookRoute = `https://bookstore-1gta.onrender.com/book`
-export const getOneBook = `https://bookstore-1gta.onrender.com/book`
-export const serviceRoute = `https://bookstore-1gta.onrender.com/service`;
+export const bookRoute = `${host}/book`
+export const getOneBook = `${host}/book`
+export const serviceRoute = `${host}/service`;
 
-export const sendOtpRoute='https://bookstore-1gta.onrender.com/user/sendotp';
-export const verifyOtpRoute = "https://bookstore-1gta.onrender.com/user/verifyotp";
+export const sendOtpRoute=`${host}/user/sendotp`;
+export const verifyOtpRoute = `${host}/user/verifyotp`;
